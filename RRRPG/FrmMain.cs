06a1ddc07eade2215e792d2cd6e456e9de06d925
Mark <amf061@latech.edu>
2023-05-04 17:50:09 -0500

    background

--- RRRPG/FrmMain.cs
+++ RRRPG/FrmMain.cs
@@ -2,166 +2,214 @@ using RRRPG.Properties;
 using RRRPGLib;
 using System.Media;
 
-namespace RRRPG {
-  public partial class FrmMain : Form {
-    private SoundPlayer soundPlayer;
-    private int state;
-    private Character player;
-    private Character opponent;
-    private Weapon weapon;
-    private Dictionary<WeaponType, (PictureBox pic, Label lbl)> weaponSelectMap;
+namespace RRRPG
+{
+    public partial class FrmMain : Form
+    {
+        private SoundPlayer soundPlayer;
+        private int state;
+        private Character player;
+        private Character opponent;
+        private Weapon weapon;
+        private Dictionary<WeaponType, (PictureBox pic, Label lbl)> weaponSelectMap;
+        private Dictionary<WeaponType, Image> weaponBackgroundMap;
 
-    public FrmMain() {
-      InitializeComponent();
-      FormManager.openForms.Add(this);
-    }
-
-    private void FrmMain_Load(object sender, EventArgs e) {
-      soundPlayer = new SoundPlayer(Resources.Mus_Title_Bg_Music);
-      soundPlayer.PlayLooping();
-      btnDoIt.Visible = false;
-      lblOpponentSpeak.Visible = false;
-      lblPlayerSpeak.Visible = false;
-      weapon = Weapon.MakeWeapon(WeaponType.MAGIC_WAND);
-      state = -1;
-      weaponSelectMap = new() {
-        {WeaponType.BOW, (picWeaponSelectBow, lblWeaponSelectBow) },
-        {WeaponType.CORK_GUN, (picWeaponSelectCorkGun,lblWeaponSelectCorkGun) },
-        {WeaponType.WATER_GUN, (picWeaponSelectWaterGun, lblWeaponSelectWaterGun) },
-        {WeaponType.MAGIC_WAND, (picWeaponSelectMagicWand, lblWeaponSelectMagicWand) },
-        {WeaponType.NERF_REVOLVER, (picWeaponSelectNerfRev, lblWeaponSelectNerfRev) },
-      };
-      SelectWeapon(WeaponType.MAGIC_WAND);
-    }
-
-    private void btnStart_Click(object sender, EventArgs e) {
-      soundPlayer.Stop();
-      player.Shutup();
-      player.ShowIdle();
-      opponent.ShowIdle();
-      btnStart.Visible = false;
-      opponent.SaySmack();
-      tmrStateMachine.Interval = 3500;
-      tmrStateMachine.Enabled = true;
-      state = 0;
-      panWeaponSelect.Visible = false;
-    }
-
-    private void tmrDialog_Tick(object sender, EventArgs e) {
-      if (state == 0) {
-        opponent.Shutup();
-        player.SaySmack();
-        state = 1;
-      }
-      else if (state == 1) {
-        opponent.Shutup();
-        player.Shutup();
-        player.ShowReady();
-        opponent.ShowNoWeapon();
-        btnDoIt.Visible = true;
-        tmrStateMachine.Enabled = false;
-        state = 2;
-      }
-      else if (state == 3) {
-        player.SayOw();
-        state = 4;
-        tmrStateMachine.Interval = 2500;
-      }
-      else if (state == 4) {
-        player.SayBoned();
-        btnStart.Visible = true;
-        tmrPlayMusicAfterGameOver.Enabled = true;
-        panWeaponSelect.Visible = true;
-        state = -1;
-        tmrStateMachine.Enabled = false;
-      }
-      else if (state == 5) {
-        player.Shutup();
-        opponent.ShowReady();
-        state = 6;
-      }
-      else if (state == 6) {
-        if (opponent.PullTrigger(weapon)) {
-          state = 7;
+        public FrmMain()
+        {
+            InitializeComponent();
+            FormManager.openForms.Add(this);
         }
-        else {
-          state = 1;
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            soundPlayer = new SoundPlayer(Resources.Mus_Title_Bg_Music);
+            soundPlayer.PlayLooping();
+            btnDoIt.Visible = false;
+            lblOpponentSpeak.Visible = false;
+            lblPlayerSpeak.Visible = false;
+            weapon = Weapon.MakeWeapon(WeaponType.MAGIC_WAND);
+            state = -1;
+            weaponSelectMap = new() {
+                {WeaponType.BOW, (picWeaponSelectBow, lblWeaponSelectBow) },
+                {WeaponType.CORK_GUN, (picWeaponSelectCorkGun,lblWeaponSelectCorkGun) },
+                {WeaponType.WATER_GUN, (picWeaponSelectWaterGun, lblWeaponSelectWaterGun) },
+                {WeaponType.MAGIC_WAND, (picWeaponSelectMagicWand, lblWeaponSelectMagicWand) },
+                {WeaponType.NERF_REVOLVER, (picWeaponSelectNerfRev, lblWeaponSelectNerfRev) },
+            };
+            weaponBackgroundMap = new() {
+                {WeaponType.BOW, Resources.Rambo },
+                {WeaponType.CORK_GUN, Resources.Rambo },
+                {WeaponType.WATER_GUN, Resources.Rambo },
+                {WeaponType.MAGIC_WAND, Resources.Rambo },
+                {WeaponType.NERF_REVOLVER, Resources.Rambo },
+            };
+            SelectWeapon(WeaponType.MAGIC_WAND);
+
+            // Add these two lines at the end
+            picOpponent.BackColor = Color.Transparent;
+            picPlayer.BackColor = Color.Transparent;
         }
-      }
-      else if (state == 7) {
-        opponent.SayOw();
-        state = 8;
-        tmrStateMachine.Interval = 2500;
-      }
-      else if (state == 8) {
-        opponent.SayBoned();
-        btnStart.Visible = true;
-        tmrPlayMusicAfterGameOver.Enabled = true;
-        panWeaponSelect.Visible = true;
-        state = -1;
-        tmrStateMachine.Enabled = false;
-      }
-    }
 
-    private void btnDoIt_Click(object sender, EventArgs e) {
-      if (player.PullTrigger(weapon)) {
-        state = 3;
-        tmrStateMachine.Interval = 2200;
-        tmrStateMachine.Enabled = true;
-      }
-      else {
-        state = 5;
-        tmrStateMachine.Interval = 1500;
-        tmrStateMachine.Enabled = true;
-      }
-      btnDoIt.Visible = false;
-    }
 
-    private void SelectWeapon(WeaponType type) {
-      Color selectedColor = Color.Yellow;
-      foreach (var weaponSel in weaponSelectMap) {
-        weaponSel.Value.pic.BackColor = Color.Black;
-        weaponSel.Value.pic.BorderStyle = BorderStyle.None;
-        weaponSel.Value.lbl.ForeColor = Color.White;
-      }
-      weaponSelectMap[type].pic.BackColor = selectedColor;
-      weaponSelectMap[type].pic.BorderStyle = BorderStyle.Fixed3D;
-      weaponSelectMap[type].lbl.ForeColor = selectedColor;
-      weapon = Weapon.MakeWeapon(type);
-      opponent = Character.MakeOpponent(type, picOpponent, lblOpponentSpeak);
-      player = Character.MakePlayer(type, picPlayer, lblPlayerSpeak);
-    }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            soundPlayer.Stop();
+            player.Shutup();
+            player.ShowIdle();
+            opponent.ShowIdle();
+            btnStart.Visible = false;
+            opponent.SaySmack();
+            tmrStateMachine.Interval = 3500;
+            tmrStateMachine.Enabled = true;
+            state = 0;
+            panWeaponSelect.Visible = false;
+        }
 
-    private void picWeaponSelectMagicWand_Click(object sender, EventArgs e) {
-      SelectWeapon(WeaponType.MAGIC_WAND);
-    }
+        private void tmrDialog_Tick(object sender, EventArgs e)
+        {
+            if (state == 0)
+            {
+                opponent.Shutup();
+                player.SaySmack();
+                state = 1;
+            }
+            else if (state == 1)
+            {
+                opponent.Shutup();
+                player.Shutup();
+                player.ShowReady();
+                opponent.ShowNoWeapon();
+                btnDoIt.Visible = true;
+                tmrStateMachine.Enabled = false;
+                state = 2;
+            }
+            else if (state == 3)
+            {
+                player.SayOw();
+                state = 4;
+                tmrStateMachine.Interval = 2500;
+            }
+            else if (state == 4)
+            {
+                player.SayBoned();
+                btnStart.Visible = true;
+                tmrPlayMusicAfterGameOver.Enabled = true;
+                panWeaponSelect.Visible = true;
+                state = -1;
+                tmrStateMachine.Enabled = false;
+            }
+            else if (state == 5)
+            {
+                player.Shutup();
+                opponent.ShowReady();
+                state = 6;
+            }
+            else if (state == 6)
+            {
+                if (opponent.PullTrigger(weapon))
+                {
+                    state = 7;
+                }
+                else
+                {
+                    state = 1;
+                }
+            }
+            else if (state == 7)
+            {
+                opponent.SayOw();
+                state = 8;
+                tmrStateMachine.Interval = 2500;
+            }
+            else if (state == 8)
+            {
+                opponent.SayBoned();
+                btnStart.Visible = true;
+                tmrPlayMusicAfterGameOver.Enabled = true;
+                panWeaponSelect.Visible = true;
+                state = -1;
+                tmrStateMachine.Enabled = false;
+            }
+        }
 
-    private void picWeaponSelectCorkGun_Click(object sender, EventArgs e) {
-      SelectWeapon(WeaponType.CORK_GUN);
-    }
+        private void btnDoIt_Click(object sender, EventArgs e)
+        {
+            if (player.PullTrigger(weapon))
+            {
+                state = 3;
+                tmrStateMachine.Interval = 2200;
+                tmrStateMachine.Enabled = true;
+            }
+            else
+            {
+                state = 5;
+                tmrStateMachine.Interval = 1500;
+                tmrStateMachine.Enabled = true;
+            }
+            btnDoIt.Visible = false;
+        }
 
-    private void picWeaponSelectWaterGun_Click(object sender, EventArgs e) {
-      SelectWeapon(WeaponType.WATER_GUN);
-    }
+        private void SelectWeapon(WeaponType type)
+        {
+            Color selectedColor = Color.Yellow;
+            foreach (var weaponSel in weaponSelectMap)
+            {
+                weaponSel.Value.pic.BackColor = Color.Transparent;
+                weaponSel.Value.pic.BorderStyle = BorderStyle.None;
+                weaponSel.Value.lbl.ForeColor = Color.White;
+            }
+            weaponSelectMap[type].pic.BackColor = selectedColor;
+            weaponSelectMap[type].pic.BorderStyle = BorderStyle.Fixed3D;
+            weaponSelectMap[type].lbl.ForeColor = selectedColor;
+            weapon = Weapon.MakeWeapon(type);
+            opponent = Character.MakeOpponent(type, picOpponent, lblOpponentSpeak);
+            player = Character.MakePlayer(type, picPlayer, lblPlayerSpeak);
+            this.BackgroundImage = weaponBackgroundMap[type];
+        }
 
-    private void picWeaponSelectNerfRev_Click(object sender, EventArgs e) {
-      SelectWeapon(WeaponType.NERF_REVOLVER);
-    }
+        private void picWeaponSelectMagicWand_Click(object sender, EventArgs e)
+        {
+            SelectWeapon(WeaponType.MAGIC_WAND);
+        }
 
-    private void picWeaponSelectBow_Click(object sender, EventArgs e) {
-      SelectWeapon(WeaponType.BOW);
-    }
+        private void picWeaponSelectCorkGun_Click(object sender, EventArgs e)
+        {
+            SelectWeapon(WeaponType.CORK_GUN);
+        }
 
-    private void FrmMain_FormClosed(object sender, FormClosedEventArgs e) {
-      FormManager.openForms.Remove(this);
-      FormManager.CloseAll();
-    }
+        private void picWeaponSelectWaterGun_Click(object sender, EventArgs e)
+        {
+            SelectWeapon(WeaponType.WATER_GUN);
+        }
 
-    private void tmrPlayMusicAfterGameOver_Tick(object sender, EventArgs e) {
-      if (btnStart.Visible) {
-        soundPlayer.PlayLooping();
-      }
-      tmrPlayMusicAfterGameOver.Enabled = false;
+        private void picWeaponSelectNerfRev_Click(object sender, EventArgs e)
+        {
+            SelectWeapon(WeaponType.NERF_REVOLVER);
+        }
+
+        private void picWeaponSelectBow_Click(object sender, EventArgs e)
+        {
+            SelectWeapon(WeaponType.BOW);
+        }
+
+        private void FrmMain_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            FormManager.openForms.Remove(this);
+            FormManager.CloseAll();
+        }
+
+        private void tmrPlayMusicAfterGameOver_Tick(object sender, EventArgs e)
+        {
+            if (btnStart.Visible)
+            {
+                soundPlayer.PlayLooping();
+            }
+            tmrPlayMusicAfterGameOver.Enabled = false;
+        }
+
+        private void picPlayer_Click(object sender, EventArgs e)
+        {
+
+        }
     }
-  }
 }
